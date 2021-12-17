b0ce458cbf497192fbbe15ff963ce5d000f7c5dc
Manueluz <manueldelauz@gmail.com>
2021-12-17 18:44:13 +0100

    Added players dropping their skulls on death

--- src/main/java/ccplugins/curseduhc/DeathHandler/DeathListener.java
+++ src/main/java/ccplugins/curseduhc/DeathHandler/DeathListener.java
@@ -7,14 +7,21 @@ import ccplugins.curseduhc.DeathHandler.DeathEnforcer.TeamEnforcer;
 import ccplugins.curseduhc.UHCGame.GameControler;
 import ccplugins.curseduhc.UHCTeams.TeamsCreator;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.block.Skull;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 
 import java.awt.*;
@@ -56,11 +63,29 @@ public class DeathListener implements Listener {
     @EventHandler
     public void onPlayerDeathEvent(PlayerDeathEvent event){
         if(!GameControler.getControler().isGameStarted()){return;}
+
+        //Register the player as dead
         deadPlayers.add(event.getEntity().getUniqueId());
         deadLocations.put(event.getEntity().getUniqueId(),event.getEntity().getLocation());
         event.setDeathMessage(ChatColor.of(new Color(255, 5, 0)) +""+ ChatColor.BOLD + "Felicidades a " + event.getEntity().getDisplayName() + " por cumplir sus sueños y morir!");
         for(Player p : plugin.getServer().getOnlinePlayers()){
             p.playSound(p.getLocation(), Sound.ENTITY_WITHER_DEATH,100,1);
         }
+
+        //Spawn his head
+        Player player = event.getEntity();
+        Block skullBlock = player.getWorld().getBlockAt(player.getLocation().add(0,1,0));
+        skullBlock.setType(Material.PLAYER_HEAD);
+        Skull skull = (Skull) skullBlock.getState();
+        skull.setOwningPlayer(Bukkit.getServer().getOfflinePlayer(player.getUniqueId()));
+        skull.update();
+        int offSet = 0;
+        do{
+            Block fence = player.getWorld().getBlockAt(player.getLocation().add(0,offSet,0));
+            fence.setType(Material.CRIMSON_FENCE);
+            offSet--;
+        }while(player.getWorld().getBlockAt(player.getLocation().add(0,offSet,0)).getBlockData().getMaterial() == Material.AIR);
+        player.getWorld().strikeLightningEffect(player.getLocation().add(0,-2,0));
+
     }
 }
