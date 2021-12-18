45c2815ea2eeca70d73b4d78f8c79aaa1da330fa
Manueluz <manueldelauz@gmail.com>
2021-12-18 02:50:55 +0100

    Everything dropped gold.

--- src/main/java/ccplugins/curseduhc/MiscFeatures/CustomDrops/GhastDrop.java
+++ src/main/java/ccplugins/curseduhc/MiscFeatures/CustomDrops/GhastDrop.java
@@ -1,6 +1,7 @@
 package ccplugins.curseduhc.MiscFeatures.CustomDrops;
 
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
@@ -13,7 +14,9 @@ public class GhastDrop implements Listener {
     Random rand = new Random();
     @EventHandler
     public void onEntityDeathEvent(EntityDeathEvent event){
-        event.getDrops().clear();
-        event.getDrops().add(new ItemStack(Material.GOLD_INGOT, rand.nextInt(2)));
+        if(event.getEntity().getType() == EntityType.GHAST){
+            event.getDrops().clear();
+            event.getDrops().add(new ItemStack(Material.GOLD_INGOT, rand.nextInt(2)));
+        }
     }
 }
