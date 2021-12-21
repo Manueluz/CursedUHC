e68f17ebf1c46fc1a724565bc80e45febfd035da
Manueluz <manueldelauz@gmail.com>
2021-12-21 15:56:45 +0100

    Added message when the vow ends

--- src/main/java/ccplugins/curseduhc/UHCGame/KnightsVow/KnightsVowFinishTask.java
+++ src/main/java/ccplugins/curseduhc/UHCGame/KnightsVow/KnightsVowFinishTask.java
@@ -1,10 +1,15 @@
 package ccplugins.curseduhc.UHCGame.KnightsVow;
 
 import ccplugins.curseduhc.UHCGame.Countdown.FinishTask;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+
+import java.awt.*;
 
 public class KnightsVowFinishTask extends FinishTask {
     @Override
     public void run() {
         KnightsVow.disable();
+        Bukkit.broadcastMessage(ChatColor.of(new Color(47, 255, 0)) + "El pacto de caballeros ha acabado, Buena suerte a todos!");
     }
 }
