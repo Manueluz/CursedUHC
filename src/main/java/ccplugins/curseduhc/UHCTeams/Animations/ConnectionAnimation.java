8c55c8f25a64f0a5cba04a6ab46adefbb7543801
Manueluz <manueldelauz@gmail.com>
2021-12-21 01:16:08 +0100

    Added player object so the end updates real time

--- src/main/java/ccplugins/curseduhc/UHCTeams/Animations/ConnectionAnimation.java
+++ src/main/java/ccplugins/curseduhc/UHCTeams/Animations/ConnectionAnimation.java
@@ -13,22 +13,19 @@ import org.bukkit.util.Vector;
 
 public class ConnectionAnimation {
     public static void animate(Player player1, Player player2, Plugin plugin){
-        Location center = player1.getLocation().toVector().midpoint(player2.getLocation().toVector()).toLocation(player1.getWorld());
-
-        new trailAnimation(player1.getLocation().add(0,1,0),center,plugin);
+        new trailAnimation(player1.getLocation().add(0,1,0),player2,plugin);
         //new trailAnimation(player2.getLocation(),center,plugin); //Teams creator will launch one from each player
-
     }
     private static class trailAnimation extends BukkitRunnable {
 
         Vector direction;
         Location current;
-        Location end;
+        Player end;
 
         int step;
 
-        public trailAnimation(Location start, Location end, Plugin plugin){
-            direction = end.subtract(start).toVector();
+        public trailAnimation(Location start, Player end, Plugin plugin){
+            direction = end.getLocation().subtract(start).toVector();
             direction = direction.multiply(1/direction.length());
 
             this.current = start;
@@ -41,12 +38,12 @@ public class ConnectionAnimation {
 
         @Override
         public void run() {
-            if(current.distance(end) < 0.5f || step > 70){
+            if( !end.isOnline() ||current.distance(end.getLocation()) < 0.5f || step > 70){
                 this.cancel();
             }
             step++;
             current = current.add(direction);
-            current.getWorld().spawnParticle(Particle.REDSTONE,current.add(0,Math.cos(current.distance(end)*3)/3f,0),8,0,0,0,0, new Particle.DustOptions(Color.BLUE,1f));
+            current.getWorld().spawnParticle(Particle.REDSTONE,current.add(0,Math.cos(current.distance(end.getLocation())*3)/3f,0),8,0,0,0,0, new Particle.DustOptions(Color.BLUE,1f));
         }
     }
 }
